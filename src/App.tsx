a82f3537e1e4a69729b5fd55e43e9c8dc52cd355
Caroline Meireles <carolinemeireles.si@gmail.com>
2025-04-28 17:42:08 -0300

    redux toolkit query

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Provider } from 'react-redux'
-import { useEffect, useState } from 'react'
 import Header from './components/Header'
 import Produtos from './containers/Produtos'
 
@@ -18,20 +17,12 @@ export type Game = {
 }
 
 function App() {
-  const [games, setGames] = useState<Game[]>([])
-
-  useEffect(() => {
-    fetch('http://localhost:4000/produtos')
-      .then((res) => res.json())
-      .then((res) => setGames(res))
-  }, [])
-
   return (
     <Provider store={store}>
       <GlobalStyle />
       <div className="container">
         <Header />
-        <Produtos jogos={games} />
+        <Produtos />
       </div>
     </Provider>
   )
